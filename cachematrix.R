3c6007fc12b7a2ecccc6c8fb98ff8e0a8b478842
saschakaelin <sascha.kaelin@zurich.com>
2014-11-22 19:16:59 +0100

    Assignment2_saschakaelin
    My solution to assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mtx = matrix()) {
+        
+        # creates a list with four functions: 
+        # save and get an invertible matrix from a local environment (cache)
+        # save its inverse to or get it from a local environment (cache)
+        
+        inv <- NULL
+        set <- function(y) {
+                mtx <<- y
+                inv <<- NULL
+        }
+        get <- function() mtx
+        setinv <- function(z) inv <<- z
+        getinv <- function() inv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cachmtx, ...) {
+        
+        # returns the inverse of a matrix
+        # checks first whether the inverse has already been calculated
+        # it is assumed that the matrix supplied to the function is invertible
+        
+        # input cachmtx : a list object returned by the makeCacheMatrix function
+        
+        inv <- cachmtx$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        
+        data <- cachmtx$get()
+        inv <- solve(data, ...)
+        cachmtx$setinv(inv)
+        inv
 }
